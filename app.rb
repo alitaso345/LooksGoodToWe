ac12a222d121f62e10b91f528bafe86840e43518
alitaso345 <alice.maru345@gmail.com>
2014-06-08 11:30:45 +0900

    画像のurlを返すための変数を定義

--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
 require_relative 'lib/LGTM'
-image_name = ARGV[0]
+require_relative 'lib/tumblr_client'
+
+image_name = ARGV[0] || TumblrClient.new("alitaso345").get_image
 LGTM.new(image_name).draw_text("LGTM")
--- lib/tumblr_client.rb
+++ lib/tumblr_client.rb
@@ -1,5 +1,9 @@
 class TumblrClient
-  def initialize
+  def initialize(user_name)
+    
+  end
 
+  def get_image
+    image_url = "http://24.media.tumblr.com/tumblr_m80zupYzDi1qc5wono1_500.gif"
   end
 end

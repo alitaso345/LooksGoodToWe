f3187c69fef266b24461027d0284cd78bbb7d6bb
alitaso345 <alice.maru345@gmail.com>
2014-06-08 09:52:57 +0900

    gifアニメに対応するようにした

--- LGTM.rb
+++ LGTM.rb
@@ -2,22 +2,25 @@ require 'RMagick'
 
 class LGTM
   def initialize(image_name)
-    @image = Magick::ImageList.new(image_name)
+    @image_list = Magick::ImageList.new(image_name)
     @draw= Magick::Draw.new
   end
 
   def draw_text(comment)
-    @draw.annotate(@image, 0, 0, 0, 0, comment) do
-      self.fill = "white"
-      self.pointsize = 100
-      self.gravity = Magick::CenterGravity
+    fontsize = @image_list.rows * 0.5 #0.5掛けだといいかんじになる
+    @image_list.each do |image|
+      @draw.annotate(image, 0, 0, 0, 0, comment) do
+        self.fill = "white"
+        self.pointsize = fontsize
+        self.gravity = Magick::CenterGravity
+      end
     end
 
-    save_image(@image)
+    save_image(@image_list)
   end
 
   def save_image(image)
-    image.write("output.jpg")
+    image.write("output.gif")
   end
 end
 

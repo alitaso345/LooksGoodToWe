82de1ba0681ede42fd4f09b4ad598ebbb03b2dee
alitaso345 <alice.maru345@gmail.com>
2014-06-10 22:59:55 +0900

    tumblrとの連携はとりあえず後回し

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require_relative 'lib/LGTM'
 require_relative 'lib/tumblr_client'
 
-image_name = ARGV[0] || TumblrClient.new("alitaso345").get_image
+image_name = ARGV[0]
 LGTM.new(image_name).draw_text("LGTM")

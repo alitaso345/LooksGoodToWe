b03274fa8351e014ea1837d20616953337c916b7
alitaso345 <alice.maru345@gmail.com>
2014-06-11 10:44:18 +0900

    tumblrを使ってもapp.rbを実行できるように修正

--- app.rb
+++ app.rb
@@ -1,5 +1,11 @@
+require "yaml"
 require_relative 'lib/LGTM'
 require_relative 'lib/tumblr_client'
 
-image_name = ARGV[0]
-LGTM.new(image_name).draw_text("LGTM")
+path = File.join(__dir__, "settings.yml")
+settings = YAML.load(open(path))
+
+images_from_tumblr = TumblrAPI.new(settings).get_image_url
+
+images = ARGV[0] || images_from_tumblr
+LGTM.new(images).draw_text("LGTM")

552101dfbbdc7f96dd15253496b68e4d45c899f4
alitaso345 <alice.maru345@gmail.com>
2014-06-11 16:11:54 +0900

    指定したユーザー名のtumblrから画像を取得できるように変更

--- lib/tumblr_client.rb
+++ lib/tumblr_client.rb
@@ -2,19 +2,19 @@ require 'tumblr_client'
 
 class TumblrAPI
   def initialize(settings)
-    tumblr = settings["tumblr"]
+    @tumblr = settings["tumblr"]
 
     Tumblr.configure do |config|
-      config.consumer_key = tumblr["consumer_key"]
-      config.consumer_secret = tumblr["consumer_secret"]
-      config.oauth_token = tumblr["oauth_token"]
-      config.oauth_token_secret = tumblr["oauth_token_secret"]
+      config.consumer_key = @tumblr["consumer_key"]
+      config.consumer_secret = @tumblr["consumer_secret"]
+      config.oauth_token = @tumblr["oauth_token"]
+      config.oauth_token_secret = @tumblr["oauth_token_secret"]
     end
   end
 
   def get_image_url()
-    hoge = Tumblr::Client.new
-    posts_info = hoge.posts("alitaso345.tumblr.com", :type => "photo", :limit => 5)
+    client = Tumblr::Client.new
+    posts_info = client.posts("#{@tumblr["user_name"]}", :type => "photo", :limit => 50)
     posts =  posts_info["posts"]
 
     #画像のURLだけを取り出してまとめる.サイズに関しては適当

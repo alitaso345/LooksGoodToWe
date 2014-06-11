25db0d6ded36b45adfadef62548fda40e4bd072b
alitaso345 <alice.maru345@gmail.com>
2014-06-11 16:15:11 +0900

    0,5掛けから0.3掛けに変更
    0.3だと縦長の画像でもLGTMの文字が収まる

--- lib/LGTM.rb
+++ lib/LGTM.rb
@@ -8,7 +8,7 @@ class LGTM
 
   def draw_text(comment)
     smaller = @image_list.rows < @image_list.columns ? @image_list.rows : @image_list.columns
-    fontsize = smaller * 0.5 #縦あるいは横幅の小さい方の0.5掛けだと大体いいかんじになる
+    fontsize = smaller * 0.3 #縦あるいは横幅の小さい方の0.3掛けだと大体いいかんじになる
     @image_list.each do |image|
       @draw.annotate(image, 0, 0, 0, 0, comment) do
         self.fill = "white"

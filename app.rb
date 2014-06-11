7897c489bb545a6556cde3a27b4c02371608e4ec
alitaso345 <alice.maru345@gmail.com>
2014-06-11 16:19:30 +0900

    コマンドラインから画像の指定できないようにした
    tumblrとの連携のみを残す

--- app.rb
+++ app.rb
@@ -5,7 +5,6 @@ require_relative 'lib/tumblr_client'
 path = File.join(__dir__, "settings.yml")
 settings = YAML.load(open(path))
 
-images_from_tumblr = TumblrAPI.new(settings).get_image_url
+images = TumblrAPI.new(settings).get_image_url
 
-images = ARGV[0] || images_from_tumblr
 LGTM.new(images).draw_text("LGTM")
